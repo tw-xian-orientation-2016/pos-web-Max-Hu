3c168e33dc758ad0052343f4b4c4bd21995503fd
Max-Hu <huzhiwise@gmail.com>
2016-03-09 18:49:36 +0800

    create data selecter

--- src/js/until.js
+++ src/js/until.js
@@ -13,3 +13,26 @@ function initData(){
     $("[name='cart-length']").text(JSON.parse(localStorage.cart).length);
     $("[name='receipt-length']").text(JSON.parse(localStorage.receipts).length);
 }
+
+function formatNumber(number) {
+    return parseFloat(number).toFixed(2);
+}
+
+function findItembyBarcode(barcode,items){
+    var item;
+    items.forEach(function(element){
+        if (element.barcode === barcode) {
+            item = element;
+        }
+    })
+    return item;
+}
+function removeItembyBarcode(barcode,items){
+    var result = [];
+    items.forEach(function(element){
+        if (element.barcode != barcode) {
+            result.push(element);
+        }
+    })
+    return result;
+}
