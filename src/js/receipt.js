8fe452037bccb8df096454e60bfb341b56319b9e
Max-Hu <huzhiwise@gmail.com>
2016-03-09 19:50:48 +0800

    implement receipt js

--- src/js/receipt.js
+++ src/js/receipt.js
@@ -1,3 +1,33 @@
-/**
- * Created by zhihu on 16/3/9.
- */
+$( document ).ready(function() {
+    initData();
+    var receipt = JSON.parse(localStorage.getItem('temple-receipt'));
+    $("[name='total-price']").text('Total Price : ' + receipt.total);
+    $("tbody").html(loadCartItemsInReceipt(receipt));
+    $("[name='home']").click(function() {
+        localStorage.removeItem('temple-receipt')
+        window.location.replace("/");
+    });
+
+});
+
+function loadCartItemsInReceipt(receipt) {
+    var cartItems = getCartItems(receipt.carItems);
+    var receiptItemsHtml = '';
+    cartItems.forEach(function(element){
+        receiptItemsHtml += createCartHtmlInRecepit(element);
+    });
+    return receiptItemsHtml
+}
+
+
+function createCartHtmlInRecepit(cartItem) {
+    var strHtml = "";
+    strHtml += '<tr>';
+    strHtml += '<td >' + cartItem.item.name + '</td>';
+    strHtml += '<td >' + cartItem.item.price + '(元)/' + cartItem.item.unit + '</td>';
+    strHtml += '<td >' + cartItem.count + ' ' + cartItem.item.unit + '</td>';
+    var subcost = cartItem.item.price*cartItem.count;
+    strHtml += '<td name="'+ cartItem.item.barcode +'">' + formatNumber(subcost) + '(元)</td>';
+    strHtml += '</tr>';
+    return strHtml;
+}
